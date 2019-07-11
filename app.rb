8b5eb4a409ee797ec371636b03c615cf91f8c4fb
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-07-11 21:00:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,6 +10,7 @@ class App < Sinatra::Base
     @team = params[:name]
     @coach = params[:coach]
     @pg = params[:pg]
+    @sg = params[:sg]
     @pf = params[:pf]
     @sf = params[:sf]
     @c = params[:c]
